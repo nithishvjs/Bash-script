1a61ccd2db9b7616955f52280f3ead6414922ec0
= <=>
2025-06-12 00:01:18 +0530

    first commit

--- shell-prep/dummy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "=== $(date) ===" >> /var/log/resource_usage.log
-echo "Memory Usage:" >> /var/log/resource_usage.log
-free -h >> /var/log/resource_usage.log
-echo "CPU Usage:" >> /var/log/resource_usage.log
-top -b -n1 | grep "Cpu(s)" >> /var/log/resource_usage.log
-echo "" >> /var/log/resource_usage.log
-
